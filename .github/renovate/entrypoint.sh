7f0f5d585407084dd8f5638f54684c87474a1676
Joseph Britton <85815005+josbritton@users.noreply.github.com>
2026-06-02 14:50:55 -0700

    Update helm version in renovate entrypoint to v4 and pin version

--- .github/renovate/entrypoint.sh
+++ .github/renovate/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3)"
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | DESIRED_VERSION=v4.1.4 bash
 runuser -u ubuntu renovate
